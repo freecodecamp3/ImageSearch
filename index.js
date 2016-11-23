366930c29648180cb560a34c3a3de5d2f5f01431
chivalry02 <chivalry02@163.com>
2016-11-23 16:46:23 +0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 function heredoc(fn) {return fn.toString().split('\n').slice(1,-1).join('\n') + '\n';}
 
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({show:false});
+
 require("http").createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	var out = 'OK';
